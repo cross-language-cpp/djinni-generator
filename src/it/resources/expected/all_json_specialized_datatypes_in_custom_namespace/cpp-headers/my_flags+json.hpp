b18079ea44ef10e05c54caf15a3e81eba0d1e7a8
Harald <harald.achitz@gmail.com>
2022-11-03 08:31:53 +0100

    Remove static keyword

--- src/it/resources/expected/all_json_specialized_datatypes_in_custom_namespace/cpp-headers/my_flags+json.hpp
+++ src/it/resources/expected/all_json_specialized_datatypes_in_custom_namespace/cpp-headers/my_flags+json.hpp
@@ -10,7 +10,7 @@
 
 namespace custom_namespace {
 
-static inline void to_json(nlohmann::json& j, my_flags e)
+inline void to_json(nlohmann::json& j, my_flags e)
 {
     static const std::pair<my_flags, nlohmann::json> m[] = {{my_flags::FLAG1,"flag1"},{my_flags::FLAG2,"flag2"}};
     j = nlohmann::json::array();
